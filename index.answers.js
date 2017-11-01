9f7fc722668f2811c6a1f7ff09073fb9abea6b95
Mike Limberg <limbergmike@gmail.com>
2017-11-01 09:35:34 -0600

    Remove parenthesis
    Removed any unnecessary parenthesis. Just a suggestion 😄

--- index.answers.js
+++ index.answers.js
@@ -1,4 +1,4 @@
-export const double = (arr) => arr.map((num) => num * 2)
-export const censor = (arr) => arr.filter((item) => item.length !== 4)
-export const allBig = (arr) => arr.every((num) => num > 1000)
-export const sum = (arr) => arr.reduce((sum, num) => sum + num, 0)
+export const double = arr => arr.map(num => num * 2)
+export const censor = arr => arr.filter(item => item.length !== 4)
+export const allBig = arr => arr.every(num => num > 1000)
+export const sum = arr => arr.reduce((sum, num) => sum + num, 0)
